e1c31e2a7a71ccf5d9abdc56e0f5326d9482c4d4
bruunosanttos <bruno-vieira-santos@hotmail.com>
2026-08-05 17:43:35 -0300

    Configura estrutura inicial da API

---
+++ __init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+from config import Config
+
+from .extensions import db, jwt, migrate
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+
+    @app.get("/health")
+    def health_check():
+        return {
+            "status": "online",
+            "message": "Hero Management API funcionando",
+        }, 200
+
+    return app
