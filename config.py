01646d86389de4cf5802412514060c39abd81d63
bruunosanttos <bruno-vieira-santos@hotmail.com>
2026-09-20 18:46:20 -0300

    fix:configura psycopg para postgre

--- config.py
+++ config.py
@@ -16,14 +16,23 @@ def get_required_env(nome):
     return valor
 
 
+database_url = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///hero_management.db",
+)
+
+if database_url.startswith("postgresql://"):
+    database_url = database_url.replace(
+        "postgresql://",
+        "postgresql+psycopg://",
+        1,
+    )
+
+
 class Config:
     SECRET_KEY = get_required_env("SECRET_KEY")
 
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        "sqlite:///hero_management.db",
-    )
-
+    SQLALCHEMY_DATABASE_URI = database_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     JWT_SECRET_KEY = get_required_env("JWT_SECRET_KEY")
